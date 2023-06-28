75068224526f97915732c997e7e393f1df336168
unknown <maretanur197@gmail.com>
2023-06-28 17:17:36 +0700

    add: all menu using redux and api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./Components/Navbar";
 import EditProfile from "./pages/EditProfile";
 import Profile from "./pages/Profile";
 import Notification from "./Components/Notification";
 import FlightTicketHistory from "./Components/FlightTicketHistory";
 import DetailHistory from "./pages/DetailHistory";
-import { Provider } from "react-redux";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 import store from "./redux/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <Navbar />
         <Routes>
           <Route path="/edit" element={<EditProfile />} />
           <Route path="/profile" element={<Profile />} />
