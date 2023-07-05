134306353390c77209aae55af6bd948e00dbe524
MaysaPutra <82851944+GMaysa@users.noreply.github.com>
2023-07-05 20:46:48 +0800

    Update index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,7 +3,6 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import searchFlightsReducers from "./searchFlightsReducers";
 import airportData from "./airportReducers";
-import auth from "./auth";
 import history from "./historyReducers";
 import profile from "./profileReducers";
 import notif from "./notifReducers";
