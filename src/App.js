cc238bcb5cac4413e92eb3beb874adea182b9940
MaysaPutra <82851944+GMaysa@users.noreply.github.com>
2023-07-05 20:16:22 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import Profile from "./pages/Profile";
 import Notification from "./Components/Notification";
 import FlightTicketHistory from "./Components/FlightTicketHistory";
 import DetailHistory from "./pages/DetailHistory";
-import Otp from "./pages/Otp";
+//import Otp from "./pages/Otp";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import SearchResults from "./pages/SearchResults";
