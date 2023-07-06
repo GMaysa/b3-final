4264f025c7168c8736f6827cf0550d2368304767
MaysaPutra <82851944+GMaysa@users.noreply.github.com>
2023-07-06 17:09:44 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
 import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Biodata from "./pages/Biodata";
+// import Biodata from "./pages/Biodata";
 import Payment from "./pages/Payment";
 import PaymentSuccess from "./pages/PaymentSuccess";
 import { Provider } from "react-redux";
