880ed1eab6c0abd9de47599a94cb40ef026fcbde
GMaysa <maysaputra0205@gmail.com>
2023-07-06 15:58:40 +0800

    update flights

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ChoosePlan from "../components/ChoosePlan";
 
-
 function Home() {
 
   return (
--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -8,51 +8,101 @@ import { MdSwapHoriz } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
 import Accordion from "../components/Accordion";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { getAllFlightSearchResult } from "../redux/actions/searchFlightsActions";
+import { thisDate } from "../components/DateView";
 
 function SearchResults() {
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const { searchResults } = useSelector((state) => state.searchFlight);
+  const [flightData, setFlightData] = useState({
+    dep: null,
+    arr: null,
+  });
 
-  const dataFromPrevPage = JSON.parse(localStorage.getItem('search_flight_data'))
+  const dataFromPrevPage = JSON.parse(
+    localStorage.getItem("search_flight_data")
+  );
 
-  const handleSubmit= (data) => {
-    localStorage.setItem('search_flight_data', JSON.stringify({flight_data:data, user_data:dataFromPrevPage}))
-  }
+  const handleSubmit = (data) => {
+    if (dataFromPrevPage.arrival) {
+      if (flightData.dep == null) {
+        setFlightData((prev) => ({ ...prev, dep: data }));
+        // dispatch(
+        //   getAllFlightSearchResult(
+        //     dataFromPrevPage.seat_class.toUpperCase(),
+        //     dataFromPrevPage.arr_airport.iata,
+        //     dataFromPrevPage.dep_airport.iata,
+        //     thisDate(dataFromPrevPage.arr_flight_date),
+        //     navigate
+        //   )
+        // )
+      } else if (flightData.arr == null) {
+        setFlightData((prev) => ({ ...prev, arr: data }));
+        navigate("/pay");
+      }
+    } else {
+      setFlightData((prev) => ({ ...prev, dep: data }));
+    }
+  };
+  localStorage.setItem(
+    "flight_data",
+    JSON.stringify({
+      flight_data: flightData,
+      user_data: dataFromPrevPage,
+    })
+  );
   // console.log(searchResults)
 
   return (
     <div className="max-w-[968px] mx-auto">
       {/* HEADER */}
-      <div className="w-full border-b-gray-300 border-b-[1.5px]">
+      <div className="w-full border-b-gray-200 border-b-[1.5px]">
         <div className="w-full pt-[130px]">
           <h1 className="font-semibold text-xl pb-6">Pilih Penerbangan</h1>
-          <div className="py-2 px-4">
+          <div className="py-2 pl-4">
             {/* Back To Landing - Bar */}
-            <div className="w-full flex gap-3 mb-[10px]">
-              <div className="w-full bg-purple-800/60 px-2 text-white py-1 rounded-lg flex items-center leading-3 font-medium" onClick={() => navigate('/')}>
+            <div className="w-full flex gap-3 mb-6">
+              <div
+                className="w-full bg-purple-800/60 px-2 text-white py-1 rounded-lg flex items-center leading-3 font-medium"
+                onClick={() =>
+                  dataFromPrevPage.arrival && flightData.dep !== null
+                    ? dispatch(
+                        getAllFlightSearchResult(
+                          dataFromPrevPage?.seat_class.toUpperCase(),
+                          dataFromPrevPage?.dep_airport.iata,
+                          dataFromPrevPage?.arr_airport.iata,
+                          thisDate(dataFromPrevPage?.arr_flight_date),
+                          navigate
+                        )
+                      ) && setFlightData((prev) => ({ ...prev, dep: null }))
+                    : navigate("/")
+                }
+              >
                 <BsArrowLeftShort size={36} />
-                &nbsp; {dataFromPrevPage.dep_airport.iata} &gt; {dataFromPrevPage.arr_airport.iata} - {dataFromPrevPage.passengers.count_passengers} Penumpang - {dataFromPrevPage.seat_class}
+                &nbsp;{" "}
+                {flightData.dep != null
+                  ? dataFromPrevPage?.arr_airport?.iata
+                  : dataFromPrevPage?.dep_airport?.iata}{" "}
+                &gt;{" "}
+                {flightData.dep != null
+                  ? dataFromPrevPage?.dep_airport?.iata
+                  : dataFromPrevPage?.arr_airport?.iata}
+                {"-"} {dataFromPrevPage?.passengers?.count_passengers} Penumpang
+                -{dataFromPrevPage?.seat_class}
               </div>
-              <div className="w-3/12 bg-lime-500/80 flex justify-center items-center text-white font-semibold py-3 rounded-lg">
-                Ubah Pencarian
-              </div>
+              <button className="flex items-center gap-2 border-[1.5px] border-purple-700 text-purple-700 rounded-lg px-4 py-2">
+                <MdSwapHoriz className="rotate-90" size={20} /> Termurah
+              </button>
             </div>
-            
           </div>
         </div>
       </div>
 
       {/* CONTENT */}
-      <div className="w-full">
-        {/* Short */}
-        <div className="w-full flex justify-end">
-          <button className="flex items-center gap-2 border-[1.5px] border-purple-700 text-purple-700 rounded-full px-4 py-2 my-6">
-            <MdSwapHoriz className="rotate-90" size={20} /> Termurah
-          </button>
-        </div>
+      <div className="w-full my-8">
         <div className="w-full flex gap-7">
           {/* Filter */}
           <div className="w-5/12 h-fit p-6 shadow-[0_0_4px_rgba(0,0,0,0.15)] rounded-[16px]">
@@ -75,7 +125,11 @@ function SearchResults() {
             {searchResults?.data?.length > 0 &&
               // searchResults?.map((data, id) => (
               searchResults?.data.map((data, id) => (
-                <Accordion data={data} key={id} onClick={() => handleSubmit(data)} />
+                <Accordion
+                  data={data}
+                  key={id}
+                  onClick={() => handleSubmit(data)}
+                />
               ))}
           </div>
         </div>
